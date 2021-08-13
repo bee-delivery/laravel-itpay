b0241fc40c60d432bf4a24ca7c27e0443af5c657
flads <flads.me@gmail.com>
2021-08-12 23:55:45 -0300

    add connection class

---
+++ src/Utils/Connection.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace BeeDelivery\ItPay\Utils;
+
+use Illuminate\Support\Facades\Http;
+
+class Connection
+{
+    protected $accessToken;
+    protected $baseUrl;
+
+    /*
+     * Pega valores no arquivo de configuração do pacote e atribui às variáveis
+     * para utilização na classe.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->accessToken = config('itpay.access_token');
+        $this->baseUrl = config('itpay.base_url');
+    }
+
+    /*
+     * Realiza uma solicitação get padrão utilizando
+     * Bearer Authentication.
+     *
+     * @param string $url
+     * @param array|null $params
+     * @return array
+     */
+    public function get($url, $params = null)
+    {
+        try {
+            $response = Http::withHeaders([
+                'Accept' => 'application/json'
+            ])
+            ->withToken($this->accessToken)
+            ->get($this->baseUrl . $url, $params);
+
+            return [
+                'code' => $response->getStatusCode(),
+                'response' => json_decode($response->getBody(), true)
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'response' => $e->getMessage()
+            ];
+        }
+    }
+
+    /*
+     * Realiza uma solicitação post padrão utilizando
+     * Bearer Authentication.
+     *
+     * @param string $url
+     * @param array|null $params
+     * @return array
+     */
+    public function post($url, $params = null)
+    {
+        try {
+            $response = Http::withHeaders([
+                'Accept' => 'application/json'
+            ])
+            ->withToken($this->accessToken)
+            ->post($this->baseUrl . $url, $params);
+
+            return [
+                'code' => $response->getStatusCode(),
+                'response' => json_decode($response->getBody(), true)
+            ];
+        } catch (\Exception $e) {
+            return [
+                'code' => $e->getCode(),
+                'response' => $e->getMessage()
+            ];
+        }
+    }
+}
