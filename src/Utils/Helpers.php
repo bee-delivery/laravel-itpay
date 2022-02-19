85287e274309255ad90e13fa5cb833914a8841d4
jean marinho <jeanmarinho529@gmail.com>
2022-02-19 13:31:42 -0300

    feature/IT-01: criando método para transformar dados em array com index id caso necessario

--- src/Utils/Helpers.php
+++ src/Utils/Helpers.php
@@ -5,5 +5,12 @@ namespace BeeDelivery\ItPay\Utils;
 
 class Helpers
 {
-    
+    public static function transformDataArrayIndexId($data)
+    {
+        if (!is_array($data)) {
+            $data = ['id' => $data];
+        }
+
+        return $data;
+    }
 }
